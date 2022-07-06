d03ef1b285f3efa9a652e3fbc4d46e0126d9591c
2022-DEV2-006 <1rjsh1@gmail.com>
2022-07-06 11:53:45 +0530

    Refactoring Setup function added to avoid duplicate initialisation of domain class

--- app/src/test/java/com/anonymous/berlinclock/BerlinClockTest.kt
+++ app/src/test/java/com/anonymous/berlinclock/BerlinClockTest.kt
@@ -4,13 +4,19 @@ import com.anonymous.berlinclock.domain.BerlinClock
 import com.anonymous.berlinclock.domain.OFF
 import com.anonymous.berlinclock.domain.YELLOW
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 
 class BerlinClockTest {
+    private lateinit var berlinClock: BerlinClock
+
+    @Before
+    fun setUp(){
+        berlinClock = BerlinClock()
+    }
 
     @Test
     fun `should return yellow state for 0 second`(){
-        val berlinClock = BerlinClock()
         val lampState = berlinClock.getLampStateForSeconds(0)
 
         assertThat(lampState).isEqualTo(YELLOW)
@@ -18,7 +24,6 @@ class BerlinClockTest {
 
     @Test
     fun `should return off state for 1 seconds`(){
-        val berlinClock = BerlinClock()
         val lampState = berlinClock.getLampStateForSeconds(1)
 
         assertThat(lampState).isEqualTo(OFF)
