35c7969e991e573cd12ed8c0b18e6cfae203c9d3
2022-DEV2-006 <1rjsh1@gmail.com>
2022-07-06 16:45:50 +0530

    Refactoring Rearranged method and order auto formatted code for readability

--- app/src/main/java/com/anonymous/berlinclock/domain/BerlinClock.kt
+++ app/src/main/java/com/anonymous/berlinclock/domain/BerlinClock.kt
@@ -5,48 +5,6 @@ import com.anonymous.berlinclock.model.BerlinClockState
 
 class BerlinClock {
 
-   private fun getLampStateForSeconds(seconds: Int): LampState = if (seconds % 2 == 0) YELLOW else OFF
-
-    private fun getLampStateForTopMinutes(minutes: Int): List<LampState> {
-        val lampState = MutableList(11) { OFF }
-        val topMinutes = minutes / 5
-        (1..topMinutes).forEach { i ->
-            if (i % 3 == 0){
-                lampState[i - 1] = RED
-            } else {
-                lampState[i - 1] = YELLOW
-            }
-        }
-        return lampState
-    }
-
-    private fun getLampStateForBottomMinutes(minutes: Int): List<LampState> {
-        val lampState = MutableList(4){OFF}
-        val bottomMinutes = minutes % 5
-        (1..bottomMinutes).forEach{ i ->
-            lampState[i - 1] = YELLOW
-        }
-        return lampState
-    }
-
-    private fun getLampStateForTopHours(hours: Int) : List<LampState> {
-        val lampState =  MutableList(4) { OFF }
-        val topHours = hours / 5
-        (1..topHours).forEach{ i ->
-            lampState[i - 1] = RED
-        }
-        return lampState
-    }
-
-    private fun getLampStateForBottomHours(hours: Int): List<LampState> {
-        val lampState = MutableList(4) { OFF }
-        val bottomHours = hours % 5
-        (1..bottomHours).forEach{ i ->
-            lampState[i - 1] = RED
-        }
-        return lampState
-    }
-
     fun getBerlinClockState(time: String): BerlinClockState {
         val timeSplit = time.split(":")
         val topHoursLampState = getLampStateForTopHours(timeSplit[0].toInt())
@@ -62,6 +20,49 @@ class BerlinClock {
             topHoursLampState,
             bottomHoursLampState
         )
+    }
+
+    private fun getLampStateForSeconds(seconds: Int): LampState =
+        if (seconds % 2 == 0) YELLOW else OFF
+
+    private fun getLampStateForTopMinutes(minutes: Int): List<LampState> {
+        val lampState = MutableList(11) { OFF }
+        val topMinutes = minutes / 5
+        (1..topMinutes).forEach { i ->
+            if (i % 3 == 0) {
+                lampState[i - 1] = RED
+            } else {
+                lampState[i - 1] = YELLOW
+            }
+        }
+        return lampState
+    }
+
+    private fun getLampStateForBottomMinutes(minutes: Int): List<LampState> {
+        val lampState = MutableList(4) { OFF }
+        val bottomMinutes = minutes % 5
+        (1..bottomMinutes).forEach { i ->
+            lampState[i - 1] = YELLOW
+        }
+        return lampState
+    }
+
+    private fun getLampStateForTopHours(hours: Int): List<LampState> {
+        val lampState = MutableList(4) { OFF }
+        val topHours = hours / 5
+        (1..topHours).forEach { i ->
+            lampState[i - 1] = RED
+        }
+        return lampState
+    }
+
+    private fun getLampStateForBottomHours(hours: Int): List<LampState> {
+        val lampState = MutableList(4) { OFF }
+        val bottomHours = hours % 5
+        (1..bottomHours).forEach { i ->
+            lampState[i - 1] = RED
+        }
+        return lampState
     }
 
 }
